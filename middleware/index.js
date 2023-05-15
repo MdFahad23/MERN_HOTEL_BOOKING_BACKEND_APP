de7463f2c6b8cbb95563afad881fd585172039b5
Md Fahad <fahadbdctglh8@gmail.com>
2023-05-15 12:30:33 +0600

    Change middleware function

--- middleware/index.js
+++ middleware/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 module.exports = (app) => {
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
   app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
+  app.use(cors());
   if (process.env.NODE_ENV === "development") {
     app.use(morgan());
   }
